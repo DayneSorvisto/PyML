52a54d01e46dffb53e46af262abc8f3517fe533a
rohithpr <praroh2@gmail.com>
2015-02-01 22:57:15 +0530

    etup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyML',
+    packages = ['pyml'],
+    version = '0.1',
+    description = 'Machine Learning algorithms in Python',
+    author = 'Rohith PR',
+    author_email = 'praroh2@gmail.com',
+    url = 'https://github.com/rohithpr/PyML',
+    download_url = 'https://github.com/rohithpr/PyML/tarball/0.1',
+    keywords = ['machine learning'],
+    classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    ],
+)
