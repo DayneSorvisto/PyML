a18e9f06ad1e07a176f20d524db093dbc0b58072
rohithpr <praroh2@gmail.com>
2015-02-01 22:06:28 +0530

    More test cases for compute cost

--- pyml/cost.py
+++ pyml/cost.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def compute_cost(x, y, theta):
-    if x.shape[1] != theta.shape[0] or (x * theta).shape[1] != theta.shape[1] or False:
+    if x.shape[1] != theta.shape[0] \
+            or x.shape[0] != y.shape[0] \
+            or x.shape[0] == 0 \
+            or False:
         return float('inf')
     raw_sum = sum(np.power((x * theta) - y, 2))
     J = int(np.sum(raw_sum, axis=1)) / ( 2 * len(y))
--- tests/test_cost.py
+++ tests/test_cost.py
@@ -15,3 +15,55 @@ def test_cost_1():
     j = compute_cost(x, y, theta)
 
     assert j == 2.3333333333333335
+
+def test_cost_2():
+    list_x = [[1, 1], [1, 2], [1, 3]]
+    list_theta = [[0], [1]]
+    list_y = [[1], [2], [3]]
+
+    x = np.matrix(list_x)
+    theta = np.matrix(list_theta)
+    y = np.matrix(list_y)
+
+    j = compute_cost(x, y, theta)
+
+    assert j == 0.0
+
+def test_cost_3():
+    list_x = [[1, 1], [1, 2], [1, 3]]
+    list_theta = [[0], [1]]
+    list_y = [[1], [2]]
+
+    x = np.matrix(list_x)
+    theta = np.matrix(list_theta)
+    y = np.matrix(list_y)
+
+    j = compute_cost(x, y, theta)
+
+    assert j == float('inf')
+
+def test_cost_4():
+    list_x = [[1, 1], [1, 2], [1, 3]]
+    list_theta = [[0]]
+    list_y = [[1], [2], [3]]
+
+    x = np.matrix(list_x)
+    theta = np.matrix(list_theta)
+    y = np.matrix(list_y)
+
+    j = compute_cost(x, y, theta)
+
+    assert j == float('inf')
+
+def test_cost_5():
+    list_x = []
+    list_theta = []
+    list_y = []
+
+    x = np.matrix(list_x)
+    theta = np.matrix(list_theta)
+    y = np.matrix(list_y)
+
+    j = compute_cost(x, y, theta)
+
+    assert j == float('inf')
