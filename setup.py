8f30485bf21b634b6bdac6b42539896275a3e336
rohithpr <praroh2@gmail.com>
2015-02-01 23:06:25 +0530

    Update setup info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'PyML',
     packages = ['pyml'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'Machine Learning algorithms in Python',
     author = 'Rohith PR',
     author_email = 'praroh2@gmail.com',
