Fix normalization

pyml/normalize_features.py
@@ -3,9 +3,9 @@ import numpy as np
 def normalize_features(x):
     temp = x[:,0]
     x = x[:,1:]
-    
-    mu = float(x.mean(axis=0))
-    sigma = float(x.std(axis=0))
+
+    mu = np.mean(x, axis=0)
+    sigma = np.std(x, axis=0)
 
     x = (x - mu) / sigma
     x = np.hstack((temp, x))