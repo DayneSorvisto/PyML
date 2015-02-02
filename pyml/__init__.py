ead554934dd92c048e853546a6ecca604b51076c
rohithpr <praroh2@gmail.com>
2015-02-02 22:26:21 +0530

    Added gradient descent

--- pyml/__init__.py
+++ pyml/__init__.py
@@ -1,2 +1,3 @@
 from pyml.cost import compute_cost
 from pyml.normalize_features import normalize_features
+from pyml.gradient_descent import gradient_descent, gradient_descent_history
---
+++ pyml/gradient_descent.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pyml
+
+def gradient_descent_base(x, y, theta, alpha, num_iters, want_history=True):
+    m = len(y)
+    cost_history = None
+    if want_history:
+        cost_history = np.zeros(num_iters)
+
+    for idx in range(num_iters):
+        temp = x.T * ((x * theta) - y);
+        theta = theta - alpha / m * temp;
+
+        if want_history:
+            cost_history[idx] = pyml.compute_cost(x, y, theta)
+    return (theta, cost_history)
+
+def gradient_descent(x, y, theta, alpha, num_iters):
+    return gradient_descent_base(x, y, theta, alpha, num_iters, want_history=False)[0]
+
+def gradient_descent_history(x, y, theta, alpha, num_iters):
+    return gradient_descent_base(x, y, theta, alpha, num_iters, want_history=True)
