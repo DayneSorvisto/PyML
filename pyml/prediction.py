e31ba944c485eb2a1d195e8d57c447750e9f2fb5
rohithpr <praroh2@gmail.com>
2015-02-05 21:21:43 +0530

    Binary classification

--- pyml/prediction.py
+++ pyml/prediction.py
@@ -1,6 +1,14 @@
+import math
 import numpy as np
 import pyml
 
 def predict(x, theta, mu, sigma):
     x = np.hstack((x[:, 0], (x[:, 1:] - mu) / sigma))
     return x * theta
+
+def binary_classification(x, theta, mu, sigma):
+	prediction = predict(x, theta, mu, sigma)
+	if 1 / (1 + math.e ** -prediction) >= 0.5:
+		return 1
+	else:
+		return 0
