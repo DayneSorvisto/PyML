0fec1f10ffcfa1d721b0f91155589b843ecc8b64
rohithpr <praroh2@gmail.com>
2015-02-06 20:08:53 +0530

    Change binary_classification to classify

--- pyml/prediction.py
+++ pyml/prediction.py
@@ -6,9 +6,6 @@ def predict(x, theta, mu, sigma):
     x = np.hstack((x[:, 0], (x[:, 1:] - mu) / sigma))
     return x * theta
 
-def binary_classification(x, theta, mu, sigma):
+def classification(x, theta, mu, sigma):
 	prediction = predict(x, theta, mu, sigma)
-	if 1 / (1 + math.e ** -prediction) >= 0.5:
-		return 1
-	else:
-		return 0
+	return 1 / (1 + math.e ** -prediction)
