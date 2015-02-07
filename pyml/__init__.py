d9dd82056fb81fbe85ceeac930eeab07e16b23b3
rohithpr <praroh2@gmail.com>
2015-02-07 21:53:46 +0530

    Update classifier

--- pyml/__init__.py
+++ pyml/__init__.py
@@ -1,6 +1,6 @@
 from pyml.cost import compute_cost
 from pyml.normalize_features import normalize_features
 from pyml.gradient_descent import gradient_descent, gradient_descent_history
-from pyml.prediction import predict
+from pyml.prediction import predict, classify
 from pyml.get_theta import get_theta
 from pyml.file_operations import load, save
--- pyml/prediction.py
+++ pyml/prediction.py
@@ -6,6 +6,6 @@ def predict(x, theta, mu, sigma):
     x = np.hstack((x[:, 0], (x[:, 1:] - mu) / sigma))
     return x * theta
 
-def classification(x, theta, mu, sigma):
+def classify(x, theta, mu, sigma):
 	prediction = predict(x, theta, mu, sigma)
 	return 1 / (1 + math.e ** -prediction)
