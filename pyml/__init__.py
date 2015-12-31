fc367fe1391471eae6e000e78573d817702f4031
Dayne <daynesorvisto@yahoo.ca>
2015-12-30 16:54:53 -0800

    Update __init__.py
    add file to __init__

--- pyml/__init__.py
+++ pyml/__init__.py
@@ -4,3 +4,4 @@ from pyml.gradient_descent import * #gradient_descent, gradient_descent_history
 from pyml.prediction import * #predict, classify
 from pyml.get_theta import * #get_theta
 from pyml.file_operations import * #load, save
+from pyml.tfidf import * #load tfidf
